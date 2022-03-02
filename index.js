6c72656c661360df8fb934412c1656c88fcc93a3
WhiteDeveloper <whitehckr@outlook.kr>
2022-03-02 23:57:34 +0900

    first-commit

---
+++ index.js
@@ -0,0 +1,12 @@
+for (var i = 0; i < 4; i++){
+    document.getElementsByClassName('a')[i].innerText = "프로그래밍"
+}
+for (var i = 0; i < 4; i++){
+    document.getElementsByClassName('b')[i].innerText = "물리학"
+}
+for (var i = 0; i < 4; i++){
+    document.getElementsByClassName('c')[i].innerText = "지구과학"
+}
+for (var i = 0; i < 4; i++){
+    document.getElementsByClassName('d')[i].innerText = "확률과 통계"
+}
